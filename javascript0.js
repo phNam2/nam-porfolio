a95b525429634451d55711764c13dabb572a5b8b
hnp435 <namhoangpham96@gmail.com>
2018-11-06 02:45:18 -0500

    Add the change widows title

---
+++ javascript0.js
@@ -0,0 +1,46 @@
+document.getElementById("gc").onclick = function() {
+    hide("container");
+    window.document.title = "LOL";
+}
+
+document.getElementById("ie").onclick = function() {
+    hide("container");
+    window.document.title = "LOL";
+}
+
+document.getElementById("mf").onclick = function() {
+    hide("container");
+    window.document.title = "LOL";
+}
+
+document.getElementById("me").onclick = function() {
+    hide("container");
+    window.document.title = "LOL";
+}
+
+document.getElementById("op").onclick = function() {
+    hide("container");
+    window.document.title = "LOL";
+}
+
+document.getElementById("vi").onclick = function() {
+    hide("container");
+    window.document.title = "LOL";
+}
+
+document.getElementById("other").onclick = function() {
+    hide("container");
+    window.document.title = "LOL";
+}
+
+// Function used to hide the properties
+function hide(id) {
+    document.getElementById(id).style.display = "none";
+    window.document.title = "LOL";
+}
+
+// Function used to show the properties
+function show(id) {
+    document.getElementById(id).style.display = "block";
+    window.document.title = "LOL";
+}
