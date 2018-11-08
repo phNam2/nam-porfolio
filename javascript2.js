e0cb0d8481022ebf85914daeaefaa321a5192316
hnp435 <namhoangpham96@gmail.com>
2018-11-08 17:34:58 -0500

    Add feature that video will turn off automatically when we go out

--- javascript2.js
+++ javascript2.js
@@ -1,3 +1,5 @@
+var vid = 0;
+
 document.getElementById("all1").onclick = function() {
     hide("all");
     show("back");
@@ -10,6 +12,7 @@ document.getElementById("all2").onclick = function() {
     show("back");
     show("project2");
     document.getElementById("works").style.background = "#9b0c0c";
+    vid = 1;
 }
 
 document.getElementById("all3").onclick = function() {
@@ -24,6 +27,7 @@ document.getElementById("all4").onclick = function() {
     show("back");
     show("project4");
     document.getElementById("works").style.background = "#9b0c0c";
+    vid = 2;
 }
 
 document.getElementById("back").onclick = function() {
@@ -34,7 +38,16 @@ document.getElementById("back").onclick = function() {
     hide("project3");
     hide("project4");
     document.getElementById("works").style.background = "radial-gradient(#0a1187, #4c43cc)";
+    
+    
+    if (vid ==1) {
+        $('#video1')[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');     
+    } else if (vid ==2) {
+        $('#video2')[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');            
+    }
 }
+
+
 
 // Function used to hide the properties
 function hide(id) {
