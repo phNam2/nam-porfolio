84cc61ac7f8aab5d67565e3f8c5b2233130184c5
hnp435 <namhoangpham96@gmail.com>
2018-11-10 11:51:41 -0500

    Current game play

--- gameAction.js
+++ gameAction.js
@@ -1,6 +1,7 @@
 var playing = false;
 var liveLeft;
 var timeRemaining;
+var actionTime;
 
 // Starting function
 $(function() {
@@ -15,12 +16,12 @@ $(function() {
             // No
             playing = true;
             
-            // Make the scoreboard appear
-            score = 0;
-            $("#score").html(score);
+//            // Make the scoreboard appear
+//            score = 0;
+//            $(".score").html(score);
             
             // Change the titile of the start button
-            $("#startReset").html("Reset");
+            $("#StartReset").html("Reset");
             
             // show the "Lives box"
 //            $("#lives").show();
@@ -41,6 +42,30 @@ $(function() {
 function addHearts() {
     $("#live").empty();
     for(i=0; i<liveLeft ; i++) {
-        $("#live").append('<img src="image/heart.gif" class="life">');
+        $("#live").append('<img src="image/game/heart.gif" class="life">');
     }
+}
+
+// Start the counting clock for the game
+function startCounting(){
+    actionTime = setInterval(function(){
+        timeRemaining -= 1;
+        $("#timeLeft").html(timeRemaining);
+        if (timeRemaining <= 0) {
+            
+            $(".score").html(score);
+            gameOver();
+        }
+    }, 1000);
+}
+
+// Stop the counting clock when the game is end 
+function stopCounting() {
+    clearInterval(actionTime);
+}
+
+
+// When the game is over
+function gameOver() {
+    
 }
